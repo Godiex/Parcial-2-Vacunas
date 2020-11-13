bf3ca1e50e069d495fd2247c0ed7f085a4c79977
Godiex <spaydiego@hotmail.com>
2020-11-12 21:57:10 -0500

    mensaje

--- ParcialVacuna/ClientApp/src/app/Parcial2/Componentes/busqueda-persona/busqueda-persona.component.ts
+++ ParcialVacuna/ClientApp/src/app/Parcial2/Componentes/busqueda-persona/busqueda-persona.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonaService } from '../../../services/persona.service';
+import { Persona } from '../../models/persona';
+import { Mensaje } from '../../../services/mensaje';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-busqueda-persona',
@@ -7,14 +10,27 @@ import { PersonaService } from '../../../services/persona.service';
   styleUrls: ['./busqueda-persona.component.css']
 })
 export class BusquedaPersonaComponent implements OnInit {
-
-  constructor(private servicioPersona: PersonaService) { }
-
+  formularioRegistro: FormGroup;
+  constructor(private servicioPersona: PersonaService, public mensaje: Mensaje, private formBuilder: FormBuilder) { }
+  identificacion: string;
+  persona: Persona = new Persona();
   ngOnInit(): void {
+    this.EstablecerValidacionesFormulario();
   }
 
   buscarPersona()
   {
+    this.servicioPersona.buscar(this.identificacion).subscribe(p => {
+      this.persona = p.elemento;
+      this.mensaje.Informar("Busqueda Persona", p.mensaje);
+    });
+  }
 
+  EstablecerValidacionesFormulario() {
+    this.formularioRegistro = this.formBuilder.group(
+      {
+        identificacion: ['', [Validators.required, Validators.minLength(10)]]
+      }
+    );
   }
 }
